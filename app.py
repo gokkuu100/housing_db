13e548823aa4afb72ccf0458100edf094c842f07
prince <wkurts247@gmail.com>
2023-10-01 19:21:19 +0300

    changed to classes

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import os
-
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 from models import db, House, Agent, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 CORS(app)
 migrate = Migrate(app, db)
 db.init_app(app)
+api = Api(app)
 
-
-@app.route('/houses', methods=['GET', 'POST'])
-def allhouses():
-    if request.method == 'GET':
+class HousesResource(Resource):
+    def get(self):
         houses = House.query.all()
         house_list = []
         for house in houses:
@@ -37,7 +35,7 @@ def allhouses():
             house_list.append(house_data)
         return jsonify(house_list), 200
 
-    elif request.method == 'POST':
+    def post(self):
         data = request.get_json()
 
         title = data.get('title')
@@ -49,7 +47,7 @@ def allhouses():
         image_paths = data.get('image_paths')
         agent_id = data.get('agent_id')
         size = data.get('size')
-        county=data["county"]
+        county = data.get('county')
 
         new_house = House(
             title=title,
@@ -77,20 +75,21 @@ def allhouses():
             'city': new_house.city,
             'agent_id': new_house.agent_id,
             'image_paths': new_house.image_paths,
-            'size': new_house.size
+            'size': new_house.size,
+            "county": new_house.county
         }
 
         return jsonify(house_data), 201
 
+api.add_resource(HousesResource, '/houses')
 
-@app.route('/houses/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
-def house_by_id(id):
-    house = House.query.filter_by(id=id).first()
+class HouseByIDResource(Resource):
+    def get(self, id):
+        house = House.query.filter_by(id=id).first()
 
-    if not house:
-        return jsonify({"error": "House not found"}), 404
+        if not house:
+            return jsonify({"error": "House not found"}), 404
 
-    if request.method == 'GET':
         house_data = {
             'id': house.id,
             'title': house.title,
@@ -101,11 +100,17 @@ def house_by_id(id):
             'city': house.city,
             'agent_id': house.agent_id,
             'image_paths': house.image_paths,
-            'county':house.county
+            'size': house.size,
+            "county": house.county
         }
         return jsonify(house_data), 200
 
-    if request.method == 'PATCH':
+    def patch(self, id):
+        house = House.query.filter_by(id=id).first()
+
+        if not house:
+            return jsonify({"error": "House not found"}), 404
+
         data = request.get_json()
 
         house.title = data.get('title', house.title)
@@ -116,72 +121,89 @@ def house_by_id(id):
         house.city = data.get('city', house.city)
         house.agent_id = data.get('agent_id', house.agent_id)
         house.image_paths = data.get('image_paths', house.image_paths)
+        house.size = data.get('size', house.size)
         house.county = data.get('county', house.county)
 
         db.session.commit()
         return jsonify({"message": "House updated successfully"}), 200
 
-    if request.method == 'DELETE':
+    def delete(self, id):
+        house = House.query.filter_by(id=id).first()
+
+        if not house:
+            return jsonify({"error": "House not found"}), 404
+
         db.session.delete(house)
         db.session.commit()
         return jsonify({"message": "House deleted successfully"}), 200
 
-@app.route('/agents', methods=['GET'])
-def agents():
-    all_agents = Agent.query.all()
-    agent_list = []
-    for agent in all_agents:
-        agent_data = {
+api.add_resource(HouseByIDResource, '/houses/<int:id>')
+
+class AgentsResource(Resource):
+    def get(self):
+        all_agents = Agent.query.all()
+        agent_list = []
+        for agent in all_agents:
+            agent_data = {
+                'id': agent.id,
+                'name': agent.name,
+                'email': agent.email,
+                'phonebook': agent.phonebook
+            }
+            agent_list.append(agent_data)
+        return jsonify(agent_list), 200
+
+api.add_resource(AgentsResource, '/agents')
+
+class AgentByIDResource(Resource):
+    def get(self, agent_id):
+        agent = Agent.query.get(agent_id)
+
+        if agent is None:
+            return jsonify({"error": "Agent not found"}), 404
+
+        response_data = {
             'id': agent.id,
             'name': agent.name,
             'email': agent.email,
-            'phonebook': agent.phonebook
+            'phonebook': agent.phonebook,
         }
-        agent_list.append(agent_data)
-    return jsonify(agent_list), 200
 
-@app.route('/agents/<int:agent_id>', methods=['GET'])
-def agent_by_id(agent_id):
-    agent = Agent.query.get(agent_id)
+        return jsonify(response_data), 200
 
-    if agent is None:
-        return jsonify({"error": "House not found"}), 404
+api.add_resource(AgentByIDResource, '/agents/<int:agent_id>')
 
-    response_data = {
-        'id': agent.id,
-        'name': agent.name,
-        'email': agent.email,
-        'phonebook': agent.phonebook,
-    }
+class UsersResource(Resource):
+    def get(self):
+        all_users = User.query.all()
+        user_list = []
+        for user in all_users:
+            user_data = {
+                'id': user.id,
+                'name': user.name,
+                'phonebook': user.phonebook
+            }
+            user_list.append(user_data)
+        return jsonify(user_list), 200
 
-    return jsonify(response_data), 200
+api.add_resource(UsersResource, '/users')
 
-@app.route('/users', methods=['GET'])
-def users():
-    all_users = User.query.all()
-    user_list = []
-    for user in all_users:
-        user_data = {
+class UserByIDResource(Resource):
+    def get(self, user_id):
+        user = User.query.get(user_id)
+
+        if user is None:
+            return jsonify({"error": "User not found"}), 404
+
+        response_data = {
             'id': user.id,
             'name': user.name,
             'phonebook': user.phonebook
         }
-        user_list.append(user_data)
-    return jsonify(user_list), 200
-@app.route('/users/<int:user_id>', methods=['GET'])
-def user_by_id(user_id):
-    user = User.query.get(user_id)
 
-    if user is None:
-        return jsonify({"error": "House not found"}), 404
+        return jsonify(response_data), 200
 
-    response_data = {
-        'id': user.id,
-        'name': user.name,
-        'phonebook': user.phonebook
-    }
-
-    return jsonify(response_data), 200
+api.add_resource(UserByIDResource, '/users/<int:user_id>')
 
 if __name__ == "__main__":
     app.run(port=5555)
